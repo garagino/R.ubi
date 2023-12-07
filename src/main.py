e9deb60e5303bfde88107a08c9949ba341a13aa1
Lucas-PC <lgbs@cesar.school>
2023-12-07 18:20:00 -0300

    some tests to conversations

--- src/main.py
+++ src/main.py
@@ -4,9 +4,27 @@ rubi = Rubi()
 
 rubi.speak('Olá pessoa! Meu nome é Rubi! E estou aqui para te ajudar!')
 
+inputs = [['olá', 'oi', 'e ai', 'opa', 'eae', 'eai', 'e ai', 'e ai rubi', 'oi rubi', 'olá rubi', 'opa rubi', 'eae rubi', 'eai rubi', 'e ai rubi'],
+          ['que horas são', 'você tem horas', 'você tem hora', 'que horas são', 'que horas são rubi', 'você tem horas rubi', 'você tem hora rubi', 'que horas são rubi'],
+          ['que dia é hoje', 'que dia é hoje rubi'],
+          ['tudo bem', 'tudo bem rubi', 'tudo bem', 'tudo bem rubi'],
+          ['tchau', 'tchau rubi', 'tchau', 'tchau rubi'],
+          ['quem é voce', 'quem é voce rubi', 'quem é você', 'quem é você rubi', 'quem é voce', 'quem é voce rubi', 'quem é você', 'quem é você rubi', 'qual seu nome'],
+          ['quais as minhas atividades para hoje']
+        ]
+outputs = [
+            ['Olá pessoa!'],
+            ['Agora são 20:30'],
+            ['Hoje é Sexta-feira, dia 17 de novembro de dois mil e vinte três'],
+            ['Estou bem, obrigada por perguntar'],
+            ['Até mais'],
+            ['Meu nome é Rubi, sou uma assistente virtual ubiqua criada no cesar.'],
+            ['Estudar, deixa de ser preguiçoso!']
+        ]
+
 while True:
     try:
-        rubi.speak(rubi.listen())
+        rubi.talk(inputs, outputs)
     except KeyboardInterrupt:
         break
 
--- src/rubi.py
+++ src/rubi.py
@@ -1,7 +1,19 @@
 from listen_and_speak import ListenAndSpeak
 from gptApi import GptApi
 
-class Rubi(ListenAndSpeak, GptApi):
+class Rubi(ListenAndSpeak):
     def __init__(self) -> None:
-        ListenAndSpeak.__init__(self)
-        GptApi.__init__(self)
+        super().__init__()
+    
+    def talk(self, inputs:list, outputs:list):
+        speech = self.listen().lower()
+        if speech == 'não entendi o que você disse':
+            return self.speak('Desculpe, não entendi o que você disse')
+        print(speech)
+        for word in inputs:
+            if speech in word:
+                return self.speak(outputs[inputs.index(word)])
+        
+        return self.speak('Desculpe, ainda não sei responder isso')
+
+rubi = Rubi()
