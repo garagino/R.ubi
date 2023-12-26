c367b775ac128844b0062d68099267b6004661e5
Lucas-PC <lgbs@cesar.school>
2023-12-26 14:54:14 -0300

    comment gptAPI

--- src/rubi.py
+++ src/rubi.py
@@ -4,4 +4,4 @@ from gptApi import GptApi
 class Rubi(ListenAndSpeak, GptApi):
     def __init__(self) -> None:
         ListenAndSpeak.__init__(self)
-        GptApi.__init__(self)
+        # GptApi.__init__(self)
