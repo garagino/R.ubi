8961e5596524df3c850cce9b0de186a93001421d
Lucas-PC <lgbs@cesar.school>
2024-01-03 17:00:23 -0300

    remove gptApi from rubi class

--- src/rubi.py
+++ src/rubi.py
@@ -1,7 +1,5 @@
 from listen_and_speak import ListenAndSpeak
-from gptApi import GptApi
 
-class Rubi(ListenAndSpeak, GptApi):
+class Rubi(ListenAndSpeak):
     def __init__(self) -> None:
         ListenAndSpeak.__init__(self)
-        # GptApi.__init__(self)
