2b1ac124c74565d703f6b4247a5134bf6909dd72
Lucas-PC <lgbs@cesar.school>
2024-03-06 13:03:58 -0300

    docs: create documentation for class Rubi

--- src/rubi.py
+++ src/rubi.py
@@ -1,17 +1,29 @@
+'''Modulo principal do Rubi'''
 from listen_and_speak import ListenAndSpeak
 from geminiApi import GeminiApi
 
 class Rubi(ListenAndSpeak, GeminiApi):
+    '''Classe que representa o Rubi'''
     def __init__(self) -> None:
         ListenAndSpeak.__init__(self)
         GeminiApi.__init__(self)
-    
+
     def conversation(self):
+        '''Método para conversar com o usuário.
+        funcionamento:
+            - Ouve a pergunta do usuário;
+            - Gera a resposta baseada na pergunta;
+            - Fala a resposta.
+            - Repete o processo até o usuário encerrar a conversa.
+        '''
         while True:
             pergunta = self.listen()
             print(pergunta)
 
-            if 'até mais' in pergunta.lower() or 'tchau' in pergunta.lower() or 'adeus' in pergunta.lower():
+            if 'até mais' in pergunta.lower() or\
+                  'tchau' in pergunta.lower() or\
+                  'adeus' in pergunta.lower():
+
                 self.speak('Até mais!')
                 break
 
