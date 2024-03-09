a0896581dfc8b1a1d593d49e8aba9e1c209ea2f5
Lucas-PC <lgbs@cesar.school>
2024-03-09 13:32:25 -0300

    create R.ubi web

--- src/app.py
+++ src/app.py
@@ -1,12 +1,15 @@
 from flask import Flask, render_template, request, jsonify
 from rubi import Rubi
+from decouple import config
 
 app = Flask(__name__)
 rubi = Rubi()
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', context={'aws_acess_key_id': config('AWS_ACCESS_KEY_ID'),
+                                                'aws_acess_secret_key': config('AWS_SECRET_ACCESS_KEY'),
+                                                'aws_region_name': 'sa-east-1'})
 
 @app.route('/conversation', methods=['POST'])
 def conversation():
@@ -16,5 +19,5 @@ def conversation():
     return jsonify({'resposta': resposta})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=False)
 
